faa4fea53da9dba816afe5da684cf973877de5e5
boyiliu1007 <119803346+boyiliu1007@users.noreply.github.com>
2024-01-18 21:50:20 +0800

    add: json file data handle

---
+++ json_data_handle.py
@@ -0,0 +1,181 @@
+import requests
+import os
+import json
+import re
+from dotenv import load_dotenv
+
+load_dotenv()
+api_key = os.getenv("Dictionary_API_key")
+WORD_FILE = './data/word.json'
+OUTPUT_FILE = './data/testOutput.json'
+translation_of_ptos = {"n.": "noun", "adj.": "adjective", "adv.": "adverb", "v.": "verb"}
+
+def get_res(word, ptos):
+    try:
+        API_URL = f'https://www.dictionaryapi.com/api/v3/references/collegiate/json/{word}?key={api_key}'
+        response = requests.get(API_URL)
+        
+        if response.status_code == 200:
+            try:
+                res = response.json()
+                if res:
+                    return res
+                else:
+                    print("No such word or pos")
+                    return None
+            except json.JSONDecodeError as json_error:
+                print(f'Error decoding JSON: {json_error}')
+                print(response.text)  # Print the response content
+                return None
+        else:
+            print(f"Error in get_res: {response.status_code}")
+            print(response.text)  # Print the error message or response content
+            return None
+
+    except Exception as e:
+        print(f'Error in get_res: {e}')
+        return None
+    
+#===================================================================================
+
+def dfs_search(json_data, target, searchlist):
+    # If it is a list
+    if isinstance(json_data, list):
+
+        findFlag = False
+        for item in json_data:
+
+            # If found target before, add the next element to the list
+            if findFlag:
+                searchlist.append(item)
+            
+            if item == target:
+                findFlag = True
+            else:
+                dfs_search(item, target, searchlist)
+
+    # If it is a dictionary
+    elif isinstance(json_data, dict):
+        for key, value in json_data.items():
+            if key == "syns" or key == "uros":
+                continue
+            if key == target:
+                searchlist.append(value)
+            else:
+                dfs_search(value, target, searchlist)
+
+#===================================================================================
+def escapeWord(sentence, wordList):
+    patterns_to_remove = ['{wi}', '{/wi}', '{qword}', '{/qword}', '{it}', '{/it}']
+
+    # Create a regular expression pattern by joining the patterns with the '|' (OR) operator
+    pattern = '|'.join(re.escape(p) for p in patterns_to_remove)
+
+    # Use re.sub() to replace matched patterns with an empty string
+    result_string = re.sub(pattern, '', sentence)
+
+    escaped_words = [re.escape(word) for word in wordList]
+    
+    # Join the escaped words with '|' (OR) for the regular expression pattern
+    pattern = r'\b(?:' + '|'.join(escaped_words) + r')\b'
+
+    # Use re.sub() to replace the matched words with underscores
+    result_string = re.sub(pattern, '_____', result_string)
+
+    return result_string
+
+
+def get_one_example(word, data, ptos):
+    example = ""
+
+    for part in data:
+        # If part['fl'] equals the desired part of speech
+        if part['fl'] == translation_of_ptos[ptos]:
+            stemList = []
+            dfs_search(part, "stems", stemList)
+            escapeWordList = stemList[0]
+            escapeWordList.append(word)
+
+            tmp = []
+            dfs_search(part, "t", tmp)
+
+            if tmp:
+                tmp = [s for s in tmp if len(s) <= 200]
+                if tmp:
+                    sentence = max(tmp, key=len)
+                else:
+                    return example
+
+                result = escapeWord(sentence, escapeWordList)
+                
+                # Find out the longest one
+                if len(result) > len(example):
+                    example = result
+
+    return example
+#===================================================================================
+
+def get_def(data, pos):
+    definition = []
+    for part in data:
+        if part['fl'] == translation_of_ptos[pos]:
+            tmp = []
+            dfs_search(part, "shortdef", tmp)
+            definition.extend(tmp)
+
+    return definition
+
+try:
+    target = "abate"
+    startFlag = False
+    target_index = 0
+    with open(WORD_FILE, 'r', encoding="utf-8") as json_file:
+        data = json.load(json_file)
+        count = 0
+        for item in data:
+            if item["word"] == target and startFlag == False:
+                startFlag = True
+            if count == 100:
+                break
+            if startFlag:
+                res = get_res(item["word"], item["pos"])
+                example = get_one_example(item["word"], res, item["pos"])
+                definition = get_def(res, item["pos"])
+                item["example"] = example
+                item["definition"] = definition
+                count += 1
+            if not startFlag:
+                target_index += 1
+
+    data = data[target_index:target_index + count]
+    try:
+    # Read existing JSON data from the file
+        with open(OUTPUT_FILE, 'r', encoding='utf-8') as json_file:
+            existing_data = json.load(json_file)
+
+        # Append new data to the existing list
+        existing_data.extend(data)
+
+        # Write the updated data back to the file
+        with open(OUTPUT_FILE, 'w', encoding='utf-8') as json_file:
+            json.dump(existing_data, json_file, ensure_ascii=False, indent=2)
+
+        print("Data appended successfully.")
+
+    except FileNotFoundError:
+        print(f'File not found: {OUTPUT_FILE}')
+    except json.JSONDecodeError as json_error:
+        print(f'Error decoding existing JSON: {json_error}')
+    except Exception as e:
+        print(f"Error: {e}")
+
+except FileNotFoundError:
+    print(f'File not found: {WORD_FILE}')
+except KeyError:
+    print('Invalid part of speech: {pos}')
+except Exception as e:
+    print(f'Error in main: {e}')
+
+
+
+
